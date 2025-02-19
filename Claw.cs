3d24388ae9b6265c73ad86d333788812f877c0e1
BlueMoonJune <bluemoonjune0403@gmail.com>
2025-02-19 10:22:35 -0800

    shrimple as that

--- Claw.cs
+++ Claw.cs
@@ -2,16 +2,39 @@ using Godot;
 using System;
 using System.Collections.Generic;
 
-public class Claw
-{
+
+public class Claw {
+
 	public Player player;
-  public List<object> behaviors;
-  public Dictionary<string, float> stats = new() {
-    {"damage", 1},
-    {"cooldown", 0.2f}
-  };
+  public List<dynamic> behaviors = new();
+  public Stats stats = new();
+
+  public float punch = 0;
+
+  public Claw(Player player) {
+    this.player = player;
+  }
 
   public void Update(float delta) {
+    GD.Print(punch, stats.cooldown);
+    punch = Mathf.MoveToward(punch, 0, delta / stats.cooldown);
+    foreach (var behavior in behaviors) {
+      behavior.Update(delta);
+    }
+  }
 
+  public void Punch() {
+    if (punch > 0) return;
+    punch = 1;
+    foreach (var behavior in behaviors) {
+      behavior.Punch();
+    }
+  }
+
+  public struct Stats {
+    public float damage = 1;
+    public float cooldown = 0.2f;
+
+    public Stats() {}
   }
 }
--- Player.cs
+++ Player.cs
@@ -9,23 +9,21 @@ public partial class Player : CharacterBody2D
   [Export]
   public float acceleration;
 
-  public Claw[] claws = new Claw[2];
+  public Claw[] claws;
 
 	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
+	public override void _Ready() {
+    claws = new Claw[] {new Claw(this), new Claw(this)};
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _PhysicsProcess(double doubelta)
-	{
+	public override void _PhysicsProcess(double doubelta) {
     var delta = (float)doubelta;
     var acc = acceleration;
 
     var movement = Input.GetVector("move_left", "move_right", "move_up", "move_down");
     if (Velocity.Dot(movement) < 0) { acc *= 2; }
     Velocity = Velocity.MoveToward(movement * speed, delta * acc * speed);
-    System.Console.WriteLine(acc);
     if (movement.Length() > 0.1) {
       Rotation = movement.Angle();
     }
@@ -35,10 +33,33 @@ public partial class Player : CharacterBody2D
     } else if (movement.Dot(Vector2.Left) > 0) {
       Scale = new Vector2(1, -1);
     }
-    ((CollisionShape2D)GetNode("Hitbox")).Rotation = -Rotation;
-    ((CollisionShape2D)GetNode("Attacks").GetNode("Punchbox")).Rotation = -Rotation;
+    ((CollisionShape2D)GetNode("Hitbox")).Rotation = -Rotation * Scale.Y;
+    ((CollisionShape2D)GetNode("Attacks").GetNode("Punchbox")).Rotation = -Rotation * Scale.Y;
 
-    GD.Print(Velocity);
     MoveAndSlide();
+
+    if (Input.IsActionJustPressed("punch_left")) {
+      claws[0].Punch();
+      GD.Print("punch_left");
+    }
+    if (Input.IsActionJustPressed("punch_right")) {
+      claws[1].Punch();
+      GD.Print("punch_right");
+    }
 	}
+
+  public override void _Process(double delta) {
+    foreach(var claw in claws) {
+      claw.Update((float)delta);
+    }
+
+    ((Node2D)GetNode(Scale.Y < 0 ? "FrontClaw" : "BackClaw")).Position = new(12 * claws[0].punch, 0);
+    ((Node2D)GetNode(Scale.Y < 0 ? "BackClaw" : "FrontClaw")).Position = new(12 * claws[1].punch, 0);
+    ((Node2D)GetNode("FrontClaw")).Position -= new Vector2(1, 0);
+  }
+
+  public void Die() {
+    Modulate = Colors.Red;
+
+  }
 }
