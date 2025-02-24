e7b7fb7e046b066037f9e4063d61002a76a406d2
BlueMoonJune <bluemoonjune0403@gmail.com>
2025-02-24 14:00:25 -0800

    spinefish movement

--- enemies/spinefish/Spinefish.cs
+++ enemies/spinefish/Spinefish.cs
@@ -3,6 +3,14 @@ using System;
 
 public partial class Spinefish : Enemy
 {
+
+	private AnimatedSprite2D sprite;
+
+	public override void _Ready() {
+		base._Ready();
+		sprite = GetNode<AnimatedSprite2D>("Sprite");
+	}
+
 	public override void _PhysicsProcess(double doubelta)
 	{
 		var delta = (float)doubelta;
@@ -13,7 +21,9 @@ public partial class Spinefish : Enemy
 			GD.PrintErr("Enemy Error: Player is null!");
 		}
 
-		var inRange = Position.DistanceTo(player.Position) <= 64;
+		var dist = Position.DistanceTo(player.Position);
+		var inRange = dist <= 64;
+		GD.Print(dist, inRange);
 
 		Velocity = Velocity.MoveToward(inRange ? Vector2.Zero : (player.Position - Position).Normalized() * speed, speed * acc * delta);
 
