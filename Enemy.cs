49186ecfb50b06d235390ffcd62b28320aa9db48
dustin <dustinb5001@gmail.com>
2025-02-24 10:47:33 -0800

    Abstracted Enemy class into MeleEnemey and RangedEnemy, RangedEnemy is not doing damage for some reason

--- Enemy.cs
+++ Enemy.cs
@@ -17,15 +17,15 @@ public partial class Enemy : CharacterBody2D {
   private float _health;
   [Export]
   public virtual float Health {
-    get => _health;
-    set {
-      if (value <= 0) {
-        Die();
-        _health = 0;
-        return;
-      }
-      _health = value;
-    }
+	get => _health;
+	set {
+	  if (value <= 0) {
+		Die();
+		_health = 0;
+		return;
+	  }
+	  _health = value;
+	}
   }
   public float hitDelay = -1;
 
@@ -34,58 +34,33 @@ public partial class Enemy : CharacterBody2D {
   public override void _Ready() {
   }
 
-  public override void _PhysicsProcess(double doubelta) {
-    var delta = (float)doubelta;
-    var acc = acceleration;
-
-    if (player == null) {
-      Modulate = Colors.Yellow;
-      GD.PrintErr("Enemy Error: Player is null!");
-    }
-
-    Velocity = Velocity.MoveToward(hitDelay <= 0 ? (player.Position - Position).Normalized() * speed : Vector2.Zero, speed * acc * delta);
-
-    if (Velocity.Dot(Vector2.Right) > 0) {
-      Scale = new Vector2(1, 1);
-    } else if (Velocity.Dot(Vector2.Left) > 0) {
-      Scale = new Vector2(1, 1);
-    }
-
-    MoveAndSlide();
-
-    if (hitDelay >= 0) {
-      hitDelay = Mathf.Max(hitDelay - delta, 0);
-      if (hitDelay <= 0 && player.Hit(this)) {
-        hitDelay = 0.25f;
-      }
-    }
-  }
+  public override void _PhysicsProcess(double doubelta) {  }
 
   public virtual void Hit(Claw source) {
-    if (hitDelay > -1) {
-      hitDelay = 0.25f;
-    }
-    Health -= source.stats.damage;
-    var dir = source.player.Transform.X;
-    GD.Print(Velocity.Slide(dir));
-    Velocity = (Velocity - source.player.Velocity).Slide(dir) + dir * source.stats.knockback + source.player.Velocity;
+	if (hitDelay > -1) {
+	  hitDelay = 0.25f;
+	}
+	Health -= source.stats.damage;
+	var dir = source.player.Transform.X;
+	GD.Print(Velocity.Slide(dir));
+	Velocity = (Velocity - source.player.Velocity).Slide(dir) + dir * source.stats.knockback + source.player.Velocity;
   }
 
   // Override per enemy class to destroy the enemy object
   public virtual void Die() {
-    GD.Print("Enemy "+name+" is dead!");
-    QueueFree();
+	GD.Print("Enemy "+name+" is dead!");
+	QueueFree();
   }
 
   public void _OnBodyEnter(Node2D body) {
-    if (body == player) {
-      hitDelay = 0.25f;
-    }
+	if (body == player) {
+	  hitDelay = 0.25f;
+	}
   }
 
   public void _OnBodyExit(Node2D body) {
-    if (body == player) {
-      hitDelay = -1;
-    }
+	if (body == player) {
+	  hitDelay = -1;
+	}
   }
 }
--- Game.cs
+++ Game.cs
@@ -17,10 +17,10 @@ public partial class Game : Node2D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-    int i = 0;
-    foreach (Node heart in healthBar.GetChildren()) {
-      ((TextureRect)heart).Modulate = Colors.White * Mathf.Clamp(player.Health - i, 0, 1);
-      i++;
-    }
+	int i = 0;
+	foreach (Node heart in healthBar.GetChildren()) {
+	  ((TextureRect)heart).Modulate = Colors.White * Mathf.Clamp(player.Health - i, 0, 1);
+	  i++;
+	}
 	}
 }
